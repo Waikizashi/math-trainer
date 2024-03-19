372507e9072987752a1969c790818bb9139843cf
Evgenii Panarin <90246528+Waikizashi@users.noreply.github.com>
2024-03-19 23:51:19 +0100

    addind vertexes

--- frontend/src/components/graphs/GraphCanvas.tsx
+++ frontend/src/components/graphs/GraphCanvas.tsx
@@ -19,20 +19,43 @@ interface GraphParams {
 }
 
 const GraphCanvas = () => {
+    const centerForceStrength = 0.005;
 
     const svgRef = useRef<any>(null);
-    const [viewBox, setViewBox] = useState('0 0 0 0'); // Начальное значение
+    const [nodes, setNodes] = useState<Node[]>([
+        { id: 0, group: 1 },
+        { id: 1, group: 2 },
+        { id: 2, group: 3 },
+        { id: 3, group: 4 },
+        { id: 4, group: 5 },
+        { id: 5, group: 6 },
+        { id: 6, group: 7 },
+    ]);
+    const [links, setLinks] = useState<Link[]>([
+        { source: 0, target: 1 },
+        { source: 1, target: 2 },
+        { source: 2, target: 3 },
+        { source: 3, target: 4 },
+        { source: 4, target: 0 },
+    ]);
+    const [viewBox, setViewBox] = useState('0 0 0 0');
+    const [svg, setSvg] = useState();
+    const [dragHandler, setDragHandler] = useState();
+    const [simulation, setSimulation] = useState();
+    const [viewBox, setViewBox] = useState();
+    const [viewBox, setViewBox] = useState();
 
-  const updateViewBox = () => {
-    if (svgRef.current) {
-      const { width, height } = svgRef.current.parentNode.getBoundingClientRect();
-      setViewBox(`0 0 ${width} ${height}`);
-    }
-  };
+    const updateViewBox = () => {
+        if (svgRef.current) {
+            const { width, height } = svgRef.current.parentNode.getBoundingClientRect();
+            setViewBox(`0 0 ${width} ${height}`);
+        }
+    };
     const view = { x: 0, y: 0 };
 
-    
     useEffect(() => {
+        const w = svgRef.current.parentNode.getBoundingClientRect().width;
+        const h = svgRef.current.parentNode.getBoundingClientRect().height;
         window.addEventListener('resize', updateViewBox);
         updateViewBox();
 
@@ -40,45 +63,30 @@ const GraphCanvas = () => {
             .attr('width', '100%')
             .attr('height', '100%');
 
-            if (svg) {    
-                const dragHandler = d3.drag<SVGSVGElement, unknown>()
-                    .on('start', function (event) {
-                        view.x = event.x;
-                        view.y = event.y;
-                    })
-                    .on('drag', function (event) {
-                        const dx = event.x - view.x;
-                        const dy = event.y - view.y;
-                        view.x = event.x;
-                        view.y = event.y;
-    
-                        // Типы для 'viewBox' должны быть числами
-                        const viewBox = svg.attr('viewBox').split(' ').map(Number);
-                        viewBox[0] -= dx;
-                        viewBox[1] -= dy;
-                        svg.attr('viewBox', viewBox.join(' '));
-                    });
-    
-                dragHandler(svg);
-            }
-        
-        const nodes: Node[] = [
-            { id: 0, group: 1 },
-            { id: 1, group: 2 },
-            { id: 2, group: 3 },
-            { id: 3, group: 4 },
-            { id: 4, group: 5 },
-            { id: 5, group: 6 },
-            { id: 6, group: 7 },
-        ];
+        if (svg) {
+            const dragHandler = d3.drag<SVGSVGElement, unknown>()
+                .on('start', function (event) {
+                    view.x = event.x;
+                    view.y = event.y;
+                })
+                .on('drag', function (event) {
+                    const dx = event.x - view.x;
+                    const dy = event.y - view.y;
+                    view.x = event.x;
+                    view.y = event.y;
 
-        const links: Link[] = [
-            { source: 0, target: 1 },
-            { source: 1, target: 2 },
-            { source: 2, target: 3 },
-            { source: 3, target: 4 },
-            { source: 4, target: 0 },
-        ];
+                    const viewBox = svg.attr('viewBox').split(' ').map(Number);
+                    viewBox[0] -= dx;
+                    viewBox[1] -= dy;
+                    svg.attr('viewBox', viewBox.join(' '));
+                });
+
+            dragHandler(svg);
+        }
+
+        // const nodes: Node[] = ;
+
+        // const links: Link[] = ;
 
 
         const linkElements = svg.append('g')
@@ -107,8 +115,13 @@ const GraphCanvas = () => {
 
         const simulation = d3.forceSimulation(nodes)
             .force('link', d3.forceLink(links).id((d: any) => d.id).distance(100))
-            .force('charge', d3.forceManyBody())
-            .force('center', d3.forceCenter(300, 300))
+            .force("charge", d3.forceManyBody()
+                .strength(-50)
+                .distanceMax(100))
+            .force('center', d3.forceCenter(
+                w / 2,
+                h / 2
+            ))
             .on('tick', () => {
                 linkElements
                     .attr('x1', (d: any) => d.source.x)
@@ -125,79 +138,82 @@ const GraphCanvas = () => {
                     .attr('y', (d: any) => d.y);
             });
 
-        // svg.on('click', function (event) {
-        //     console.log(event)
-        //     const coords = d3.pointer(event, this); // Получаем координаты клика относительно SVG
+        svg.on('click', (event) => {
+            // Преобразуем координаты клика в координаты SVG с учетом текущего viewBox
+            const coords = d3.pointer(event);
+            const newNode: Node = {
+                id: nodes.length,
+                group: Math.floor(Math.random() * 10), // Пример группы для нового узла
+                x: coords[0],
+                y: coords[1],
+            };
+            setNodes(prevNodes => [...prevNodes, newNode]);
+            console.log(nodes)
+            updateSimulation();
+        });
+        const updateSimulation = () => {
+            svg.selectAll('g > *').remove();
+            simulation.nodes(nodes);
+            // const linkForce = simulation.force("link", d3.forceLink(links).id((d: any) => d.id));
 
-        //     // // Создаем новый узел в месте клика
-        //     const newNode: Node = { id: nodes.length, x: coords[0], y: coords[1] };
-        //     nodes.push(newNode); // Добавляем новый узел в массив узлов
+            const link = svg.select<SVGAElement>('g')
+                .selectAll<SVGLineElement, Link>('line')
+                .data(links, (d: any) => `${d.source.id}-${d.target.id}`);
+            link.enter()
+                .append('line')
+                .style('stroke', '#aaa')
+                .merge(link);
+            link.exit().remove();
 
-        //     // // Перезапускаем симуляцию с новыми узлами
-        //     // simulation.nodes(nodes);
-        //     // (simulation.force('link') as d3.ForceLink<any, any>).links(links);
-        //     // simulation.alpha(1).restart();
-
-        //     updateGraph(); // Функция для обновления графа
-        // });
-        const addVertex = () => {
-
-        }
-
-        const deleteVertex = () => {
-
-        }
-
-        const cleanAll = () => {
-
-        }
-
-        const showDegree = () => {
-
-        }
-
-        const showDegreeIndex = () => {
-
-        }
-
-        const setColors = () => {
-
-        }
-
-        function updateGraph() {
-            // Обновляем узлы
-            const nodeSelection = svg.selectAll<SVGCircleElement, Node>('circle')
+            const node = svg.select('g')
+                .selectAll<SVGCircleElement, Node>('circle')
                 .data(nodes, (d: any) => d.id);
-
-            nodeSelection.enter()
+            node.enter()
                 .append('circle')
                 .attr('r', 10)
-                // другие атрибуты для узлов...
-                .merge(nodeSelection)
-                .attr('cx', (d: any) => d.x)
-                .attr('cy', (d: any) => d.y);
+                .style('fill', (d: any) => colorScale(d.group))
+                .merge(node);
+            node.exit().remove();
 
-            nodeSelection.exit().remove();
+            const text = svg.select('g')
+                .selectAll<SVGTextElement, Node>('text')
+                .data(nodes, (d: any) => d.id);
+            text.enter()
+                .append('text')
+                .attr('dx', 12)
+                .attr('dy', '.35em')
+                .text(d => d.id)
+                .style('font-size', '12px')
+                .merge(text);
+            text.exit().remove();
 
-            // Обновляем связи
-            const linkSelection =  svg.selectAll<SVGLineElement, Link>('line.link')
-            .data(links);
+            simulation.alpha(0.3).restart();
+        };
+    }, [nodes]);
 
-            linkSelection.enter()
-                .append('line')
-                // другие атрибуты для связей...
-                .merge(linkSelection)
-                .attr('x1', (d: any) => d.source.x)
-                .attr('y1', (d: any) => d.source.y)
-                .attr('x2', (d: any) => d.target.x)
-                .attr('y2', (d: any) => d.target.y);
+    const addVertex = () => {
 
-            linkSelection.exit().remove();
+    }
 
-            // Перезапускаем симуляцию, если это необходимо
-            // simulation.nodes(nodes).alpha(1).restart();
-        }
-    }, []);
+    const deleteVertex = () => {
+
+    }
+
+    const cleanAll = () => {
+
+    }
+
+    const showDegree = () => {
+
+    }
+
+    const showDegreeIndex = () => {
+
+    }
+
+    const setColors = () => {
+
+    }
 
     const canvasStyles = cn(
         'graph-canvas',
