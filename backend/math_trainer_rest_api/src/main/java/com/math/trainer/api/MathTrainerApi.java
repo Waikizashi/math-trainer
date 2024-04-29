b6bceabb8d398beba4e34da3cb0e5a6650ed12d0
Evgenii Panarin <90246528+Waikizashi@users.noreply.github.com>
2024-04-29 21:55:22 +0200

    backend init

---
+++ backend/math_trainer_rest_api/src/main/java/com/math/trainer/api/MathTrainerApi.java
@@ -0,0 +1,13 @@
+package com.mtrainer.math_trainer_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathTrainerRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathTrainerRestApiApplication.class, args);
+	}
+
+}
---
+++ backend/math_trainer_rest_api/src/test/java/com/mtrainer/math_trainer_rest_api/MathTrainerRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtrainer.math_trainer_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathTrainerRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
